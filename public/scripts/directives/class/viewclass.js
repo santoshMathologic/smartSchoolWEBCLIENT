3cfc67662b1008a7bed2342e3a5f0679b926a245
santosh <santosh.citech@gmail.com>
2016-05-31 23:00:08 -0700

    view Class added with real time data

--- public/scripts/directives/class/viewclass.js
+++ public/scripts/directives/class/viewclass.js
@@ -5,10 +5,26 @@ angular.module('smartSchoolApp')
 			templateUrl: 'scripts/directives/class/viewclass.html',
 			restrict: 'EA',
 			replace: true,
-			controller: function ($scope) {
+			controller: function ($scope, $http, UserAuthFactory, AuthenticationFactory) {
 				console.log("Inside view class diredctive Controller")
-				
-				
+
+
+
+				$scope.query = {
+                    order: 'class_Name',
+                    limit: 10,
+                    page: 1,
+
+                };
+
+				$http.get(apiUrl + "/" + "class" + "/" + "classes", { params: $scope.query })
+					.then(function (response) {
+						$scope.classList = response.data.results;
+					});
+
+
+
+
 
 
 			},

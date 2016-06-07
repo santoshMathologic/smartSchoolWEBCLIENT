858b4e0af8a576d48c39a2fc98ea41e4bf0f9f84
santosh <santosh.citech@gmail.com>
2016-06-07 11:38:37 -0700

    angular Tooltips added and UserPlan form has been designed Successfully

--- public/scripts/directives/userPlan/userPlan.js
+++ public/scripts/directives/userPlan/userPlan.js
@@ -1,27 +1,41 @@
 'use strict'
 
-angular.module('smartSchoolApp').directive('userPlan',function(){
-    
-    return{
-          templateUrl :'scripts/directives/userPlan/userPlan.tmpl.html',
-          restrict:'EA',
-          replace:true,
-          controller: function($scope,$http){
-            
-            console.log("Scope vales",$scope);
-              
-          },
-          link :function(scope,element,attrs){
-              
-              
-              
-          }
+angular.module('smartSchoolApp').directive('userPlan', function () {
+
+  return {
+    templateUrl: 'scripts/directives/userPlan/userPlan.tmpl.html',
+    restrict: 'EA',
+    replace: true,
+    controller: function ($scope, $http) {
+      
+      $scope.queryParam = {
+        sortBy:'planName',
+        limit:20,
+        page:1
         
+      }
+      
+      $scope.userPlanLists = [];
+
+      $http.get(apiUrl + "/plans/getPlans",{params:$scope.queryParam}).then(function(res){
+        $scope.userPlanLists = res.data.results;
         
+      }); 
+
+      console.log("Scope vales", $scope);
+
+    },
+    link: function (scope, element, attrs) {
+
+
+
     }
-    
-    
-    
+
+
+  }
+
+
+
 });
 
 

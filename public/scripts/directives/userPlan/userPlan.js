9ea44ff669b0d40dbc8b7fac2bce0f76ebaa03f5
santosh <santosh.citech@gmail.com>
2016-06-07 18:51:17 -0700

    modal added

--- public/scripts/directives/userPlan/userPlan.js
+++ public/scripts/directives/userPlan/userPlan.js
@@ -47,6 +47,18 @@ console.log("Scope vales", $scope);
       restrict: 'E',
       transclude: true,
       scope: {},
+       template:
+        '<div class="tabbable">' +
+          '<ul class="nav nav-tabs">' +
+            '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
+              '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
+            '</li>' +
+          '</ul>' +
+          '<div class="tab-content" ng-transclude></div>' +
+        '</div>',
+      replace: true,
+
+      
       controller: [ "$scope", function($scope) {
         var panes = $scope.panes = [];
  
@@ -62,16 +74,7 @@ console.log("Scope vales", $scope);
           panes.push(pane);
         }
       }],
-      template:
-        '<div class="tabbable">' +
-          '<ul class="nav nav-tabs">' +
-            '<li ng-repeat="pane in panes" ng-class="{active:pane.selected}">'+
-              '<a href="" ng-click="select(pane)">{{pane.title}}</a>' +
-            '</li>' +
-          '</ul>' +
-          '<div class="tab-content" ng-transclude></div>' +
-        '</div>',
-      replace: true
+     
     };
   }).
   directive('pane', function() {

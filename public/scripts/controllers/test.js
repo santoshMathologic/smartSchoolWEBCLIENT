3df564869c8bb300ad42a59bc3c8af3f855bcba6
santosh <santosh.citech@gmail.com>
2016-06-09 19:35:09 -0700

    test Pages added

--- public/scripts/controllers/test.js
+++ public/scripts/controllers/test.js
@@ -1,7 +1,22 @@
 'use strict';
 angular.module('smartSchoolApp')
-    .controller('testCtrl', function ($scope, $state, $position, $q, $window) {
+    .controller('testCtrl', function ($scope, $state, $position, $q, $window, $http) {
 
-         console.log("Test Page");    
+        console.log("Test Page");
+        $scope.userLists = [];
+
+        $http.get("http://jsonplaceholder.typicode.com/posts").then(function (res) {
+
+            $scope.userLists = res.data;
+
+        })
+
+        $scope.remove = function (user) {
+            $scope.userLists.splice($scope.userLists.indexOf(user.Name), 1);
+        }
+
+        $scope.update = function () {
+
+        }
 
     });
